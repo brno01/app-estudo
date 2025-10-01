19aeed4adfa0b29e3da48a3b4984c6188ee88ca3
brno01 <brunohashtagfox@gmail.com>
2025-09-30 23:14:43 -0300

    0.0.2

--- app.js
+++ app.js
@@ -1,24 +1,23 @@
 const express = require('express');
 const app = express();
+const apiV1ProductRouter = require('./router/api/v1/product');
 
-const productRouter = require('./router/product');
+app.use('/api/v1/products', apiV1ProductRouter);
 
-app.use('/apiv1/products', productRouter);
-
-app.use('/apiv1', (req, res, next) => {
-    res.status(200).send({
+app.use('/api/v1', (req, res) => {
+    res.status(200).json({
         message: 'Tudo funcionando!'
     });
-    res.status(404).send({
+    res.status(404).json({
         message: 'Rota não encontrada!'
     });
-    res.status(500).send({
+    res.status(500).json({
         message: 'Erro interno do servidor!'
     });
-    res.status(400).send({
+    res.status(400).json({
         message: 'Requisição inválida!'
     });
-    res.status(401 || 403).send({
+    res.status(401 || 403).json({
         message: 'Não autorizado!'
     });
     next();
