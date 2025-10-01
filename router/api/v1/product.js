7ac59145b965f109eedd745422f16aa2b2082745
brno01 <brunohashtagfox@gmail.com>
2025-09-30 23:18:01 -0300

    0.0.3

--- router/api/v1/product.js
+++ router/api/v1/product.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-    res.status(200).send({
+    res.status(200).json({
         message: 'Lista de produtos'
     });
 });
@@ -10,40 +10,40 @@ router.get('/', (req, res, next) => {
 router.get('/:id', (req, res, next) => {
     const id = req.params.id;
     if (!id) {
-        return res.status(400).send({
+        return res.status(400).json({
             message: 'ID do produto é obrigatório!'
         });
     }
     if (id === 'especial') {
-        return res.status(200).send({
+        return res.status(200).json({
             message: 'Produto especial encontrado!'
         });
     }
     if (id === 'not_found') {
-        return res.status(404).send({
+        return res.status(404).json({
             message: 'Produto não encontrado!'
         });
     } else {
-        res.status(200).send({
+        res.status(200).json({
             message: `Produto encontrado com ID: ${id}`
         });
     }
 });
 
 router.post('/', (req, res, next) => {
-    res.status(201).send({
+    res.status(201).json({
         message: 'Produto criado com sucesso!'
     });
 });
 
 router.patch('/:id', (req, res, next) => {
-    res.status(200).send({
+    res.status(200).json({
         message: 'Produto atualizado com sucesso!'
     });
 });
 
 router.delete('/:id', (req, res, next) => {
-    res.status(204).send({
+    res.status(204).json({
         message: 'Produto removido com sucesso!'
     });
 });
