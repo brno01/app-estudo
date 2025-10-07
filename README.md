2a74a2f2a170c1349845e73229f41c0d449c8caf
brno01 <brunohashtagfox@gmail.com>
2025-10-06 23:07:42 -0300

    docker completed

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 docker-compose up -d --build
+
+docker ps
+
+docker exec -it postgres_db psql -U root -d app-db
+
+\dt
--- internal/db/db.js
+++ internal/db/db.js
@@ -3,11 +3,11 @@ import pkg from 'pg';
 const { Pool } = pkg;
 
 const pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
+    user: process.env.POSTGRES_USER,
+    host: process.env.POSTGRES_HOST,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+    port: process.env.POSTGRES_PORT,
     max: 20,
     idleTimeoutMillis: 30000,
     connectionTimeoutMillis: 2000
@@ -18,10 +18,10 @@ pool.connect()
     .then(client => {
         console.log({
             message: 'Rodando! ✅',
-            host: process.env.DB_HOST,
-            port: process.env.DB_PORT,
-            database: process.env.DB_NAME,
-            user: process.env.DB_USER
+            host: process.env.POSTGRES_HOST,
+            port: process.env.POSTGRES_PORT,
+            database: process.env.POSTGRES_DB,
+            user: process.env.POSTGRES_USER
         });
         console.log({ message: 'Conectado ao Postgres com sucesso! ✅' });
         client.release();
